451496771b16896b03e72d2b24de360917a1e67b
Olivier Aubert <contact@olivieraubert.net>
2015-11-20 14:53:27 +0100

    Remove whitespace

--- src/widgets/AdaptivePlayer.js
+++ src/widgets/AdaptivePlayer.js
@@ -11,17 +11,17 @@ IriSP.Widgets.AdaptivePlayer.prototype.defaults = {
 };
 
 IriSP.Widgets.AdaptivePlayer.prototype.draw = function() {
-    
+
     if (typeof this.video === "undefined") {
         this.video = this.media.video;
     }
-    
+
     var _props = [ "autostart", "video", "height", "width", "url_transform" ],
         _opts = {},
         _canPlayType = document.createElement('video').canPlayType(this.mime_type);
-    
+
     _opts.type = (_canPlayType !== "no") ? this.normal_player : this.fallback_player;
-    
+
     for (var i = 0; i < _props.length; i++) {
         if (typeof this[_props[i]] !== "undefined") {
             _opts[_props[i]] = this[_props[i]];
@@ -29,5 +29,4 @@ IriSP.Widgets.AdaptivePlayer.prototype.draw = function() {
     }
 
     this.insertSubwidget(this.$, _opts);
-    
 };
